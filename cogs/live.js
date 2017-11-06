d8323a9aafbff4c2af5d1e6697fa85ca21bab7a5
Crazybmanp <crazybmanp@gmail.com>
2017-11-05 22:04:43 -0600

    added more logic to adding streamers

--- cogs/live.js
+++ cogs/live.js
@@ -6,6 +6,7 @@ const datapath = datadirectory + datafile;
 
 var bot = {};
 var data = {};
+var subcommands = {};
 
 var loadData = function () {
     var contents = fs.readFileSync(datapath);
@@ -24,17 +25,61 @@ if(fs.existsSync(datapath))
     if (!fs.existsSync(datadirectory)) {
         fs.mkdirSync(datadirectory);
     }
-    data = {"Twitch":[], "ServerSettings":[]};
+    data = {"Twitch":{}, "ServerSettings":[]};
     saveData();
 }
 
+var addStreamer = function (msg) {
+    var command = msg.content.split(" ");
+    var server = sg.guild.id;
+    var stream = command[0];
+    if(typeof command[1] === 'undefined') {
+        var level = 1;
+    }else{
+        var level = command[1];
+    }
 
+    if(!(stream in data.twitch))
+    {
+        data.twitch[stream]=[];
+    }
+    data.twitch[stream].push({
+        "server": server,
+        "level": level
+    })
+    saveData();
+    msg.reply("Added streamer to list");
+}
+subcommands["add"] = getQuote;
+
+var configServer = function (msg) {
+    var command = msg.content.split(" ");
+    var obj = {
+        "server": msg.guild.id,
+        "announceChannel": command[0]
+    }
+    data.ServerSettings.push(obj);
+    msg.reply("configured server settings!");
+}
+subcommands["config"] = getQuote;
+
+var liveHandler = function (msg) {
+    var command = msg.content.split(" ")[0];
+    msg.content = msg.content.substr(command.length + 1, msg.content.length);
+    if (command === "") {
+        command = ""
+    };
+    var fn = subcommands[command];
+    if (typeof fn === 'function') {
+        fn(msg);
+    } else {
+        msg.reply("Cannot find subcommand... [" + command + "]");
+    }
+}
 
 var setup = function (b) {
     bot = b;
-    // bot.registerCommand("ping", function (msg) {
-    //     msg.reply('Pong!')
-    // });
+    bot.registerCommand("live", liveHandler);
 }
 
 exports.requires = [];
