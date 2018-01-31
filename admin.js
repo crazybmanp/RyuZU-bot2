d7d6fc5a2ef9d24cb2d6cab9a4fc2d6bd0c374f9
Brentton Paulus <crazybmanp@gmail.com>
2018-01-31 02:25:45 -0600

    Removed requirement of mod to clean

--- admin.js
+++ admin.js
@@ -33,23 +33,17 @@ var say = function(msg)
 var clean = function(msg)
 {
     msg.channel.startTyping();
-    if(bot.isMod(msg.channel, msg.author))
-    {
-        lim = parseInt(msg.content);
-        msg.channel.fetchMessages({limit: isNaN(lim) ? 100 : lim})
-        .then(function(messages){
-            var messages = messages.filter(function(s){
-                return (s.author.id===bot.client.user.id || s.content.startsWith(bot.config.commandString))
-            })
-            msg.channel.bulkDelete(messages);
-            d = msg.reply("Deleted " + messages.size + " messages.");
-            msg.channel.stopTyping();
+    lim = parseInt(msg.content);
+    msg.channel.fetchMessages({limit: isNaN(lim) ? 100 : lim})
+    .then(function(messages){
+        var messages = messages.filter(function(s){
+            return (s.author.id===bot.client.user.id || s.content.startsWith(bot.config.commandString))
         })
-        .catch(console.error);
-    } else {
-        msg.reply("You are not allowed to do that");
+        msg.channel.bulkDelete(messages);
+        d = msg.reply("Deleted " + messages.size + " messages.");
         msg.channel.stopTyping();
-    }
+    })
+    .catch(console.error);
 }
 
 var purge = function(msg)
